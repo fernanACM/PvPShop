3a2ada7b85f0a7cc6f1296d91bd7d28b66699789
fernanACM <83558341+fernanACM@users.noreply.github.com>
2023-01-27 05:18:05 -0500

    new subcommand "help"

--- src/fernanACM/PvPShop/commands/ShopCommand.php
+++ src/fernanACM/PvPShop/commands/ShopCommand.php
@@ -1,12 +1,22 @@
 <?php
 
+#      _       ____   __  __ 
+#     / \     / ___| |  \/  |
+#    / _ \   | |     | |\/| |
+#   / ___ \  | |___  | |  | |
+#  /_/   \_\  \____| |_|  |_|
+# The creator of this plugin was fernanACM.
+# https://github.com/fernanACM
+
 namespace fernanACM\PvPShop\commands;
+
+use pocketmine\player\Player;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 use fernanACM\PvPShop\Loader;
 use fernanACM\PvPShop\utils\PluginUtils;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\player\Player;
 
 class ShopCommand extends Command{
 
@@ -39,6 +49,17 @@ class ShopCommand extends Command{
         }
 
         switch($args[0]){
+            case "help":
+            case "?":
+                $sender->sendMessage("§l§e=====( PVPSHOP )=====");
+                $sender->sendMessage("§7/pvpshop help - Command list");
+                $sender->sendMessage("§7/pvpshop amor - Open the armor category");
+                $sender->sendMessage("§7/pvpshop tools - Open the tools category");
+                $sender->sendMessage("§7/pvpshop food - Open the food category");
+                $sender->sendMessage("§7/pvpshop extra - Open the extra category");
+                PluginUtils::PlaySound($sender, "random.pop", 1, 1);
+            break;
+                
             case "armor":
                 if(!$sender->hasPermission("pvpshop.category.armor")){
                     $sender->sendMessage(Loader::Prefix(). Loader::getMessage($sender, "Messages.no-permission"));
@@ -47,7 +68,7 @@ class ShopCommand extends Command{
                 }
 
                 Loader::getArmorMenu()->getArmorMenuGUI($sender);
-                PluginUtils::PlaySound($sender, "random.chestopen");
+                PluginUtils::PlaySound($sender, "random.chestopen", 1, 1);
             break;
 
             case "food":
@@ -58,7 +79,7 @@ class ShopCommand extends Command{
                 }
 
                 Loader::getFoodMenu()->getFoodMenuGUI($sender);
-                PluginUtils::PlaySound($sender, "random.chestopen");
+                PluginUtils::PlaySound($sender, "random.chestopen", 1, 1);
             break;
 
             case "extra":
@@ -69,7 +90,7 @@ class ShopCommand extends Command{
                 }
 
                 Loader::getExtraMenu()->getExtraMenuGUI($sender);
-                PluginUtils::PlaySound($sender, "random.chestopen");
+                PluginUtils::PlaySound($sender, "random.chestopen", 1, 1);
             break;
 
             case "tools":
@@ -80,7 +101,7 @@ class ShopCommand extends Command{
                 }
 
                 Loader::getToolsMenu()->getToolsMenuGUI($sender);
-                PluginUtils::PlaySound($sender, "random.chestopen");
+                PluginUtils::PlaySound($sender, "random.chestopen", 1, 1);
             break;
 
             default:
